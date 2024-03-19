0dd116f9626b42fb513c293abad9019c30509c47
HabibBehdarvandiDev <habibbehdarvandi.dev@gmail.com>
2024-03-19 16:35:22 +0330

    add banner to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { BiStar } from "react-icons/bi";
 
 export default function Home() {
   return (
     <main>
-      
+      <p className="w-full bg-primary text-white p-2 flex justify-between items-center">
+        <span>
+          <BiStar className="w-5 h-5 m-2" />
+        </span>
+        <span>ساخته شده برای شما | Made For You</span>
+        <span>
+          <BiStar className="w-5 h-5 m-2" />
+        </span>
+      </p>
     </main>
   );
 }
